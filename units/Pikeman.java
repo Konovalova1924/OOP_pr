8f65a2d7052d175bf4ee6d1ab7e2fb8ef9dcf2bd
Nadya <gadiarrr30@gmail.com>
2024-03-20 21:30:59 +0300

    first seminar

---
+++ units/Pikeman.java
@@ -0,0 +1,12 @@
+package units;
+// Копейщик
+public class Pikeman extends Person {
+    
+    public Pikeman(String name, int age){
+        super(name, 80, 30, age, 30, 50, "pike", 20);
+    }
+    public void attack(Person person){
+        person.health -= this.power;
+    }
+
+}
