5e369ef04ed4e45c9ba251638f88cc5072a18f8f
Nadya <gadiarrr30@gmail.com>
2024-03-25 13:47:56 +0300

    Created teams, added coordinates, added for sniper findNearestPerson

--- units/Robber.java
+++ units/Robber.java
@@ -1,9 +1,9 @@
 package units;
 // Разбойник
 public class Robber extends Person {
-    public Robber (String name, int age){
-        super(name, 100, 50, age, 20, 40, 
-        "knife", 50);        
+    public Robber (String name, int x, int y){
+        super(name, 100, 50, 28, 20, 40, 
+        "knife", 50, "Разбойник", x, y);        
     }
     public void attack(Person person){
         person.health -= this.power;
