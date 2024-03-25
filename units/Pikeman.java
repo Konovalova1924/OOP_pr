66f9f1f7e15f97b0a9b43b2eaf274f6343040ddd
Nadya <gadiarrr30@gmail.com>
2024-03-25 13:48:36 +0300

    Created teams, added coordinates, added for sniper findNearestPerson

--- units/Pikeman.java
+++ units/Pikeman.java
@@ -2,13 +2,11 @@ package units;
 // Копейщик
 public class Pikeman extends Person {
     
-    public Pikeman(String name, int age){
-        super(name, 80, 30, age, 30, 50, "pike", 20);
+    public Pikeman(String name, int x, int y){
+        super(name, 80, 30, 41, 30, 50, "pike", 20, "Копейщик", x, y);
     }
     public void attack(Person person){
         person.health -= this.power;
     }
-    public String toString() {
-        return String.format("Name: %s Class: %s" , name, this.getClass().getSimpleName());
-    }
+    
 }
