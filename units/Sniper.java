8a473bfd23870bff21393b4bb58e434d75bba8e8
Nadya <gadiarrr30@gmail.com>
2024-03-25 13:47:42 +0300

    Created teams, added coordinates, added for sniper findNearestPerson

--- units/Sniper.java
+++ units/Sniper.java
@@ -5,9 +5,9 @@ public class Sniper extends Person {
     private int arrowNum;
     boolean inGame = true;
     
-    public Sniper(String name, int age) {
-        super(name, 60, 40, age, 20, 40, 
-        "gun", 30);
+    public Sniper(String name, int x, int y) {
+        super(name, 60, 40, 33, 20, 40, 
+        "gun", 30, "Снайпер", x, y);
     }
     public void attack(Person person){
         person.health -= this.power;
@@ -15,4 +15,5 @@ public class Sniper extends Person {
     public boolean isInGame() {
         return this.arrowNum == 0 ? false : true;
     }
+     
 }
