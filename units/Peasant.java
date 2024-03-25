f22ba4871dade9bc24c4e1fe1d67b08a83b7c4d6
Nadya <gadiarrr30@gmail.com>
2024-03-25 13:49:08 +0300

    Created teams, added coordinates, added for sniper findNearestPerson

--- units/Peasant.java
+++ units/Peasant.java
@@ -3,9 +3,9 @@ package units;
 public class Peasant extends Person {
     private int arrowNum;
     boolean inGame = true;
-    public Peasant(String name, int age){
-        super(name, 20, 5, age, 0, 20, 
-        "nothing", 50);
+    public Peasant(String name, int x, int y){
+        super(name, 20, 5, 44, 0, 20, 
+        "nothing", 50, "Крестьянин", x, y);
     }
     public void giveArrows(int val) {
         this.arrowNum -= val;
