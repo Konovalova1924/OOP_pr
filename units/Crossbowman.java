b5acc6ed7cc428d7508813608503857ecef78959
Nadya <gadiarrr30@gmail.com>
2024-03-25 13:49:47 +0300

    Created teams, added coordinates, added for sniper findNearestPerson

--- units/Crossbowman.java
+++ units/Crossbowman.java
@@ -2,9 +2,9 @@ package units;
 // Арбалетчик
 public class Crossbowman extends Person{
     
-    public Crossbowman(String name, int age) {
-        super(name, 60, 40, age, 20, 40, 
-        "crossbow", 30);
+    public Crossbowman(String name, int x, int y) {
+        super(name, 60, 40, 38, 20, 40, 
+        "crossbow", 30, "Арбалетчик", x, y);
     }
     
     public void attack(Person person){
